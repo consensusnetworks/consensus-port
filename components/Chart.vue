bfc5b31cc9fc78c5076f3f88a159c3fab455b364
demogorgod <ssnajeeb2020@manchester.edu>
2022-01-13 23:23:23 -0500

    Create the dashboard for the Explorer page

--- components/Chart.vue
+++ components/Chart.vue
@@ -1,14 +1,48 @@
 <template>
-  <canvas id="chart" width="10" height="200px"></canvas>
+  <!-- 
+    I have found that the best way to make the Chart reactive
+    is to use the height prop that the Chart component provides and 
+    use the parent component to control the width. 
+   -->
+  <canvas :id="chartID" :height="height"></canvas>
 </template>
 
 <script setup>
 import Chart from "chart.js/auto";
 
 const props = defineProps({
+  chartID: {
+    type: String,
+    required: true,
+  },
+  type:{
+    type: String,
+    required: true,
+    default: 'line'
+  },
   chartData: {
     type: Object,
     required: true,
+  },
+  height: {
+    type: String,
+    required: true,
+    default: '100'
+  },
+  title: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  xTitle: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  yTitle: {
+    type: String,
+    required: false,
+    default: ""
   },
 });
 
@@ -16,18 +50,45 @@ let ctx = null;
 let chart = null;
 
 const options = {
+  responsive: true,
+  animtion: false,
   scales: {
+    x: {
+      display: true,
+      grid: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: props.xTitle
+      },
+    },
     y: {
-      beginAtZero: true,
+      display: true,
+      grid: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: props.yTitle
+      }
+    },
+  },
+  plugins: {
+    title: {
+      display: true,
+      text: props.title
     },
   },
 };
 
+
 // Init to run in mounted hook to guarantee client access
 onMounted(() => {
-  ctx = document.getElementById("chart").getContext("2d");
+  ctx = document.getElementById(props.chartID).getContext("2d");
   chart = new Chart(ctx, {
     data: props.chartData,
+    type: props.type,
     options,
   });
 });
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-secondary min-h-screen h-full">
+  <div class="min-h-screen h-full">
     <NavBar />
     <!-- Todo: fix SSR hydration issue and remove client-only wrapper -->
     <client-only>
